1318148d42a225f2722ca47e0691690dc9bf27da
Robin Berjon <robin@berjon.com>
2013-09-06 13:37:15 +0200

    testing different branches

--- app.js
+++ app.js
@@ -1,8 +1,1 @@
-var express = require("express")
-,   app = express()
-;
-
-app.get("/", function(req, res){
-    res.send("<h1>dynamic ohai!</h1>");
-});
-app.listen(process.env.PORT);
+console.log("Does nothing");
